4a284df149abab2f3b82d29ed261c78863a5fdcc
Konstantin Möllers <ksm.moellers@gmail.com>
2022-10-19 21:07:21 +0200

    feat: implement service struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use hyper::service::{make_service_fn, service_fn};
+use hyper::service::{make_service_fn, Service};
 use hyper::{Body, Request, Response, Server};
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use std::{convert::Infallible, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(Svc { count: 0 }) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
@@ -15,7 +18,24 @@ async fn main() {
     }
 }
 
-async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let msg = format!("{}, World!", req.method());
-    Ok(Response::new(msg.into()))
+struct Svc {
+    count: u32,
+}
+
+impl Service<Request<Body>> for Svc {
+    type Response = Response<Body>;
+    type Error = hyper::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        println!("I am being called for {:?}", req);
+        self.count += 1;
+        let msg = format!("{}, {}!", req.method(), self.count);
+        let res = Ok(Response::new(msg.into()));
+        Box::pin(async { res })
+    }
 }
