67d47b0c2367efaa728c3c992fb455ee9314e348
Fernando Porazzi <fernandoporazzi@gmail.com>
2022-12-08 11:59:51 +0100

    Add executed_at to Trade model

--- src/model/trade.rs
+++ src/model/trade.rs
@@ -1,5 +1,6 @@
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::model::OrderId;
 
@@ -9,6 +10,7 @@ pub struct Trade {
     pub quantity: Decimal,
     pub buy_order_id: OrderId,
     pub sell_order_id: OrderId,
+    pub executed_at: u128,
 }
 
 impl Trade {
@@ -18,11 +20,15 @@ impl Trade {
         buy_order_id: OrderId,
         sell_order_id: OrderId,
     ) -> Self {
+        let now = SystemTime::now();
+        let executed_at = now.duration_since(UNIX_EPOCH).unwrap().as_nanos();
+
         Self {
             price,
             quantity,
             buy_order_id,
             sell_order_id,
+            executed_at,
         }
     }
 }
